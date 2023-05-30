9c340b876042da107ff8b19af7b5365511e69f75
Apizarro <apizarro1204@gmail.com>
2023-05-30 17:42:41 -0400

    update css

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -6,10 +6,33 @@ export default function Characters(props) {
       <span className="back-home">Back to Home</span>
       <div className="container-characters">
         {characters.map((character, index) => {
-          return(
-          <div className="character-container" key={index}>
-            <p>{character.name}</p>
-          </div>)
+          return (
+            <div className="character-container" key={index}>
+              <div>
+                <img src={character.image} alt={character.name} />
+              </div>
+              <div>
+                <h3>{character.name}</h3>
+                <h6>
+                  {character.status === "Alive" ? (
+                    <>
+                      <span className="alive" />
+                      Alive
+                    </>
+                  ) : (
+                    <>
+                      <span className="dead" />
+                      Dead
+                    </>
+                  )}
+                </h6>
+                <p>
+                  <span className='text-grey'>Episodios:</span>
+                  <span>{character.episode.length}</span>
+                  </p>
+              </div>
+            </div>
+          );
         })}
       </div>
     </div>
